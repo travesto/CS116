07aee686f6a452e0c59c2111e1c3d109689dcd99
Travis Cook <tdc12c@acu.edu>
2017-09-03 15:46:10 -0500

    started dateconverter
    I hate python

---
+++ dateconverter.py
@@ -0,0 +1,10 @@
+"""
+Date converter 
+"""
+months = ['January', 'February', '"Marzo', 'Abril', 'May', 'June', 'July', 'Agosto', 'September', 'October', 'November', 'December']
+
+print('This program converts dates.')
+date = input('Please enter a date in the form mm/dd/yyyy: ')
+
+converted = str(months[dateList[:1]] + " " + dateList[3:4] + ", " + dateList[6:])
+print(converted)
---
+++ phonenumber.py
@@ -0,0 +1,14 @@
+"""
+Telephone converter
+"""
+letters = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+
+phone = str(input("Please enter an alphabetic telephone number:"))
+
+print('The converted telephone number is ')
+
+for n in phone:
+    if phone[n].isalpha():
+        for m in range
+    else:
+        print('np')
